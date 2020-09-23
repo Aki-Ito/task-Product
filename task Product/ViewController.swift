8ef96b45c99277bb0a4754d4c4421c1ff2419e49
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2020-09-24 00:11:19 +0900

    Initial Commit

---
+++ task Product/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task Product
+//
+//  Created by 伊藤明孝 on 2020/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
